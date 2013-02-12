c992e1bcf0a9b9031a9a908eecfba936364f82bb
PanteR <panter.dsd@gmail.com>
2013-02-12 12:00:38 +0400

    Translation is added.

---
+++ share/translations/WordsCounter_ru.ts
@@ -0,0 +1,95 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru">
+<context>
+    <name>FileParser</name>
+    <message>
+        <location filename="../../src/fileparser.cpp" line="95"/>
+        <source>Line longer that %1 symbols</source>
+        <translation>Строка длиннее %1 символов</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="14"/>
+        <location filename="../../src/mainwindow.cpp" line="122"/>
+        <source>Words counter</source>
+        <translation>Счетчик слов</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="27"/>
+        <source>Select file...</source>
+        <translation>Выбрать файл...</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="34"/>
+        <location filename="../../src/mainwindow.cpp" line="85"/>
+        <source>Start calculate</source>
+        <translation>Запустить подсчет</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="211"/>
+        <location filename="../../src/mainwindow.cpp" line="219"/>
+        <location filename="../../src/mainwindow.cpp" line="224"/>
+        <source>Save result</source>
+        <translation>Сохранение результата</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="41"/>
+        <source>Save result...</source>
+        <translation>Сохранить результат...</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="51"/>
+        <source>Progress</source>
+        <translation>Ход работы</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="66"/>
+        <source>Worked:</source>
+        <translation>Прошло:</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="80"/>
+        <source>Words count:</source>
+        <translation>Количество слов:</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="94"/>
+        <source>ETA:</source>
+        <translation>Осталось:</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="113"/>
+        <source>Results</source>
+        <translation>Результаты</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="84"/>
+        <source>Stop calculate</source>
+        <translation>Остановить подсчет</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="104"/>
+        <location filename="../../src/mainwindow.cpp" line="114"/>
+        <source>Open file</source>
+        <translation>Открыть файл</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="115"/>
+        <source>File not select or size is 0</source>
+        <translation>Файл не выбран или его размер равен нулю</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="220"/>
+        <source>Result is saved</source>
+        <translation>Результат сохранен</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="225"/>
+        <source>Error save result</source>
+        <translation>Ошибка сохранения результата</translation>
+    </message>
+</context>
+</TS>
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
  * @date 10.02.2013
  */
 
+#include <QtCore/QTranslator>
+#include <QtCore/QLocale>
+
 #include <QtGui/QApplication>
 
 #include "mainwindow.h"
@@ -10,6 +13,10 @@
 int main (int argc, char **argv)
 {
 	QApplication app (argc, argv);
+
+	QTranslator translator;
+	translator.load (":share/translations/WordsCounter_ru.qm");
+	QApplication::installTranslator (&translator);
 
 	MainWindow win;
 	win.show();
